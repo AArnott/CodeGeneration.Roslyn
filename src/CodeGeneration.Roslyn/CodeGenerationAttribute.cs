13e142eea7d495a8432d01f9ff7051403525dc82
Andrew Arnott <andrewarnott@gmail.com>
2015-12-12 11:58:15 -0800

    Add initial project

---
+++ src/CodeGeneration.Roslyn/CodeGenerationAttribute.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace CodeGeneration.Roslyn
+{
+    using System;
+
+    /// <summary>
+    /// A base attribute type for code generation attributes.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class CodeGenerationAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeGenerationAttribute"/> class.
+        /// </summary>
+        /// <param name="generatorFullTypeName">
+        /// The fully-qualified type name (including assembly information)
+        /// of the code generator to activate.
+        /// </param>
+        public CodeGenerationAttribute(string generatorFullTypeName)
+        {
+            this.GeneratorFullTypeName = generatorFullTypeName;
+        }
+
+        /// <summary>
+        /// Gets the fully-qualified type name (including assembly information)
+        /// of the code generator to activate.
+        /// </summary>
+        public string GeneratorFullTypeName { get; }
+    }
+}
