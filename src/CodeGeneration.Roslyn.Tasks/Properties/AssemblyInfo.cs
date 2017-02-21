e1046b07f57ba4748dac789f7508985f07d94604
Andrew Arnott <andrewarnott@gmail.com>
2017-02-20 21:40:08 -0800

    Convert all class library projects to .NET SDK

--- src/CodeGeneration.Roslyn.Tasks/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Andrew Arnott. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("CodeGeneration.Roslyn.Tasks")]
-[assembly: AssemblyProduct("CodeGeneration.Roslyn.Tasks")]
-[assembly: AssemblyCopyright("Copyright © Andrew Arnott 2015")]
