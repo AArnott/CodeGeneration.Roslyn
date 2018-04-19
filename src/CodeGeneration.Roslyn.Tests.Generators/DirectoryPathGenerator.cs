366c0187ef019e76284607446030ee5745762021
Psilon <pzixel@gmail.com>
2018-04-19 18:24:21 +0300

    Format enhancement

--- src/CodeGeneration.Roslyn.Tests.Generators/DirectoryPathGenerator.cs
+++ src/CodeGeneration.Roslyn.Tests.Generators/DirectoryPathGenerator.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 
-using System;
-
 namespace CodeGeneration.Roslyn.Tests.Generators
 {
+    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
@@ -22,29 +21,21 @@ namespace CodeGeneration.Roslyn.Tests.Generators
 
         public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
         {
-            var results = SyntaxFactory.List<MemberDeclarationSyntax>();
-
-            var compilationUnitSyntax = (CompilationUnitSyntax) context.ProcessingMember;
-            MemberDeclarationSyntax copy = ClassDeclaration("DirectoryGenerationTest")
+            var member = ClassDeclaration("DirectoryGenerationTest")
                 .AddMembers(
-                            FieldDeclaration(
-                                             VariableDeclaration(
-                                                                 PredefinedType(
-                                                                                Token(SyntaxKind.StringKeyword)))
-                                                 .AddVariables(
-                                                               VariableDeclarator(
-                                                                                  Identifier("S"))
-                                                                   .WithInitializer(
-                                                                                    EqualsValueClause(
-                                                                                                      LiteralExpression(
-                                                                                                                        SyntaxKind.StringLiteralExpression,
-                                                                                                                        Literal(context.ProjectDirectory))))))
-                                .WithModifiers(
-                                               TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.ConstKeyword))));
+                    FieldDeclaration(
+                        VariableDeclaration(
+                            PredefinedType(Token(SyntaxKind.StringKeyword)))
+                        .AddVariables(
+                            VariableDeclarator(Identifier("S"))
+                            .WithInitializer(
+                                EqualsValueClause(
+                                    LiteralExpression(
+                                        SyntaxKind.StringLiteralExpression,
+                                        Literal(context.ProjectDirectory))))))
+                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.ConstKeyword))));
 
-            results = results.Add(copy);
-
-            return Task.FromResult(results);
+            return Task.FromResult(List<MemberDeclarationSyntax>(new []{member}));
         }
     }
 }
