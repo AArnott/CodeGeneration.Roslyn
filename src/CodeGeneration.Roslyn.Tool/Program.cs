f0aedc236cf5497d59f88334f4726e618c52815c
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-01-21 13:14:33 +0100

    Fixed copyright note, added root files to sln

--- src/CodeGeneration.Roslyn.Tool/Program.cs
+++ src/CodeGeneration.Roslyn.Tool/Program.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 
 namespace CodeGeneration.Roslyn.Generate
 {
